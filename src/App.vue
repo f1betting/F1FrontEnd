6541a1700eb1f54fc84dad2379787d598102d7b2
Niek <Frankenstein795@gmail.com>
2022-09-20 15:44:00 +0200

    Remove unused stuff

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 <template>
   <div class="w-screen h-screen bg-blue-600 flex justify-center place-items-center">
-    <!--    <div id="g_id_onload"-->
-    <!--        :data-client_id="CLIENT_ID"-->
-    <!--        data-context="use"-->
-    <!--        data-ux_mode="popup"-->
-    <!--        :data-callback="log"-->
-    <!--        data-auto_select="true"-->
-    <!--        data-itp_support="true">-->
-    <!--    </div>-->
-
-    <!--    <div class="g_id_signin"-->
-    <!--        data-type="standard"-->
-    <!--        data-shape="pill"-->
-    <!--        data-theme="filled_black"-->
-    <!--        data-text="continue_with"-->
-    <!--        data-size="large"-->
-    <!--        data-logo_alignment="left">-->
-    <!--    </div>-->
     <div id="buttonDiv"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-/// <reference types="google.accounts" />
-import { onMounted } from "vue";
-
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-const BASE_URL  = import.meta.env.BASE_URL;
 
 function handleCredentialResponse(response: any) {
   console.log(response);
