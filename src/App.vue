df625b739b12467e1046588c0cf75c48946685f4
Niek <Frankenstein795@gmail.com>
2022-09-21 11:54:28 +0200

    Move user info to sidebar

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,29 @@
       <SignInButton />
     </Card>
 
-    <Card class="justify-between py-10" v-if="userStore.id.sub">
+    <div class="bg-red w-64 h-screen p-5 absolute left-0 flex flex-col place-items-center text-white drop-shadow-2xl"
+        v-if="userStore.id.sub">
       <img :src="userStore.id.picture"
           alt="PROFILE PICTURE"
-          class="w-24 h-auto place-self-center rounded-full drop-shadow-xl">
+          class="w-24 h-auto rounded-full drop-shadow-xl">
 
-      <div class="text-center">
+      <div class="text-white">
         <h1 class="font-bold">{{ userStore.id.name }}</h1>
         <h1 class="font-bold">{{ userStore.id.sub }}</h1>
         <h1 class="font-bold">{{ userStore.id.email }}</h1>
       </div>
 
-      <button class="w-full bg-off-white text-black" @click="signOut">Sign out</button>
-    </Card>
+      <div class="absolute bottom-0 p-5 text-white">
+        <img src="https://logodownload.org/wp-content/uploads/2016/11/formula-1-logo-5-3.png"
+            alt="F1 LOGO"
+            class="w-full h-auto brightness-0 invert">
+        <h1 class="text-center text-4xl font-bold">F1 BETTING</h1>
+
+        <button class="w-full bg-white text-red text-2xl rounded-md drop-shadow-lg font-bold mt-5"
+            @click="signOut">SIGN OUT
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
